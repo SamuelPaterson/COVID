9657163e1050b9b74833010066a7b4a3f5185cba
SamuelPaterson <sam.joe.pat@gmail.com>
2022-06-07 14:09:38 +0100

    Added link to Comparison Dashboard

--- COVID_SQLQuery.sql
+++ COVID_SQLQuery.sql
@@ -1,3 +1,30 @@
+SELECT * INTO temp_continents
+FROM COVID..All_Data
+WHERE continent = ''
+	AND location NOT LIKE '%income%'
+	AND NOT location = 'International'
+	AND NOT location = 'European Union'
+	AND location != 'World';
+
+
+-- Location Comparison Data
+SELECT location, date, total_cases, total_deaths, population, people_vaccinated, people_fully_vaccinated, total_boosters
+	FROM COVID..All_Data
+	ORDER BY location, date
+
+-- For comparing Covid reponses
+WITH cte AS (
+	SELECT location,
+		(MAX(total_cases)/MAX(population))*100 AS percentage_contracted_covid,
+		(MAX(total_deaths)/MAX(population))*100 AS percentage_population_death
+	FROM temp_continents
+	GROUP BY location
+	)
+SELECT location, percentage_contracted_covid,
+	(percentage_population_death/percentage_contracted_covid)*100 AS percentage_death_rate
+	FROM cte;
+
+
 -- Vaccinations vs % deaths per case for each continent
 -- Used to evaluate the effectiveness of vaccinations
 SELECT date, location,
@@ -7,12 +34,7 @@ SELECT date, location,
 	CASE
 		WHEN SUM(new_cases) != 0 THEN (SUM(new_deaths)/SUM(new_cases))*100
 	END AS death_percentage
-FROM All_Data
-WHERE continent = ''
-	AND location NOT LIKE '%income%'
-	AND NOT location = 'International'
-	AND NOT location = 'European Union'
-	AND location != 'World'
+FROM temp_continents
 GROUP BY date, location, population
 ORDER BY location, date;
 
@@ -22,7 +44,7 @@ ORDER BY location, date;
 SELECT location, population,
 	(MAX(total_cases)/population)*100 AS percentage_contracted_covid,
 	(MAX(total_deaths)/MAX(total_cases))*100 AS percentage_deaths_percase
-FROM All_Data
+FROM COVID..All_Data
 WHERE continent = 'Europe' AND total_cases>0
 GROUP BY location, population
 ORDER BY population;
@@ -31,6 +53,6 @@ ORDER BY population;
 -- New cases each day vs tests conducted
 -- Used to evaluate effect of mass testing on case numbers.
 SELECT date, SUM(new_cases) AS cases_today, SUM(new_tests) AS tests_today
-FROM All_Data
+FROM COVID..All_Data
 GROUP by date
 ORDER BY date;
---
+++ readme.md
@@ -0,0 +1,7 @@
+## Covid-19 Comparison Dashboard
+
+###### Data was filtered and manipulated using Microsoft SQL Server Manager.
+###### All data analysis was performed with Tableau. [Tableau Public - Covid Comparison](https://public.tableau.com/app/profile/samuel.paterson/viz/COVIDComparison_16541843739140/Dashboard1?publish=yes)
+
+
+![](DashboardScreenshot.png)
